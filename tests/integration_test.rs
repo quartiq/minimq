b5511eeeebcfb9b1ac427016b0698667ae805062
Ryan Summers <ryan.summers@vertigo-designs.com>
2021-08-20 13:20:44 +0200

    Fixing format

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,7 +9,8 @@ fn main() -> std::io::Result<()> {
 
     let stack = std_embedded_nal::Stack::default();
     let localhost = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    let mut mqtt = Minimq::<_, _, 256>::new(localhost, "", stack, StandardClock::default()).unwrap();
+    let mut mqtt =
+        Minimq::<_, _, 256>::new(localhost, "", stack, StandardClock::default()).unwrap();
 
     let mut published = false;
     let mut subscribed = false;
