9de0e6f659c82deb61cadb92f3e510e8fa628fa7
Ryan Summers <ryan.summers@vertigo-designs.com>
2023-08-14 12:57:00 +0200

    Adding integration test for client reconnections

---
+++ tests/reconnection.rs
@@ -0,0 +1,93 @@
+use minimq::{
+    types::{SubscriptionOptions, TopicFilter},
+    Minimq, Publication, QoS,
+};
+
+use embedded_nal::{self, IpAddr, Ipv4Addr};
+use std_embedded_time::StandardClock;
+
+mod stack;
+
+#[test]
+fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    let mut rx_buffer = [0u8; 256];
+    let mut tx_buffer = [0u8; 256];
+    let mut session = [0u8; 256];
+    let sockets = std::cell::RefCell::new(Vec::new());
+    let stack = stack::MitmStack::new(&sockets);
+    let localhost = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let mut mqtt = Minimq::new(
+        localhost,
+        "",
+        stack,
+        StandardClock::default(),
+        &mut rx_buffer,
+        &mut tx_buffer,
+        &mut session,
+    )
+    .unwrap();
+
+    // Use a keepalive interval for the client.
+    mqtt.client().set_keepalive_interval(1).unwrap();
+
+    // 1. Poll until we're connected and subscribed to a test topic
+    while !mqtt.client().is_connected() {
+        mqtt.poll(|_client, _topic, _payload, _properties| {})
+            .unwrap();
+    }
+
+    let topic_filter = TopicFilter::new("test")
+        .options(SubscriptionOptions::default().maximum_qos(QoS::ExactlyOnce));
+    mqtt.client().subscribe(&[topic_filter], &[]).unwrap();
+
+    while mqtt.client().subscriptions_pending() {
+        mqtt.poll(|_client, _topic, _payload, _properties| {})
+            .unwrap();
+    }
+
+    // 2. Send a QoS::AtLeastOnce message
+    mqtt.client()
+        .publish(
+            Publication::new("Ping".as_bytes())
+                .topic("test")
+                .qos(QoS::ExactlyOnce)
+                .finish()
+                .unwrap(),
+        )
+        .unwrap();
+
+    // Force a disconnect from the broker.
+    for socket in sockets.borrow_mut().iter_mut() {
+        socket.1.close();
+    }
+
+    // 3. Wait until the keepalive timeout lapses and we disconnect from the broker.
+    while mqtt.client().is_connected() {
+        mqtt.poll(|_client, _topic, _payload, _properties| {})
+            .unwrap();
+    }
+
+    assert!(mqtt.client().pending_messages());
+
+    // 4. Poll until we're reconnected
+    while !mqtt.client().is_connected() {
+        mqtt.poll(|_client, _topic, _payload, _properties| {})
+            .unwrap();
+    }
+
+    // 5. Verify that we finish transmission of our pending message.
+    let mut rx_messages = 0;
+    while mqtt.client().pending_messages() && rx_messages == 0 {
+        mqtt.poll(|_client, _topic, _payload, _properties| {
+            rx_messages += 1;
+        })
+        .unwrap();
+    }
+
+    // 5. Verify that we receive the message after reconnection
+    assert!(rx_messages == 1);
+
+    Ok(())
+}
---
+++ tests/stack/mod.rs
@@ -0,0 +1,162 @@
+use embedded_nal::{nb, TcpClientStack};
+use std::cell::RefCell;
+use std::io::{Error, ErrorKind, Read, Write};
+use std::net::{SocketAddr, TcpStream};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct TcpHandle {
+    id: usize,
+}
+
+pub struct MitmStack<'a> {
+    pub sockets: &'a RefCell<Vec<(TcpHandle, TcpSocket)>>,
+    handle: usize,
+}
+
+impl<'a> MitmStack<'a> {
+    pub fn new(sockets: &'a RefCell<Vec<(TcpHandle, TcpSocket)>>) -> Self {
+        Self { sockets, handle: 0 }
+    }
+
+    pub fn add_socket(&mut self) -> TcpHandle {
+        let handle = TcpHandle { id: self.handle };
+        self.handle += 1;
+        self.sockets.borrow_mut().push((handle, TcpSocket::new()));
+        handle
+    }
+
+    pub fn close(&mut self, handle: TcpHandle) {
+        let index = self
+            .sockets
+            .borrow_mut()
+            .iter()
+            .position(|(h, _sock)| h == &handle)
+            .unwrap();
+        self.sockets.borrow_mut().swap_remove(index);
+    }
+}
+
+#[derive(Debug)]
+pub struct TcpError(pub Error);
+
+impl TcpError {
+    fn broken_pipe() -> TcpError {
+        TcpError(Error::new(ErrorKind::BrokenPipe, "Connection interrupted"))
+    }
+}
+
+fn to_nb(e: std::io::Error) -> nb::Error<TcpError> {
+    match e.kind() {
+        ErrorKind::WouldBlock | ErrorKind::TimedOut => nb::Error::WouldBlock,
+        _ => nb::Error::Other(TcpError(e)),
+    }
+}
+
+impl From<Error> for TcpError {
+    fn from(e: Error) -> Self {
+        Self(e)
+    }
+}
+
+impl embedded_nal::TcpError for TcpError {
+    fn kind(&self) -> embedded_nal::TcpErrorKind {
+        match self.0.kind() {
+            std::io::ErrorKind::BrokenPipe => embedded_nal::TcpErrorKind::PipeClosed,
+            _ => embedded_nal::TcpErrorKind::Other,
+        }
+    }
+}
+
+pub struct TcpSocket {
+    stream: Option<TcpStream>,
+}
+
+impl TcpSocket {
+    fn new() -> Self {
+        Self { stream: None }
+    }
+
+    fn connect(&mut self, remote: embedded_nal::SocketAddr) -> Result<(), Error> {
+        let embedded_nal::IpAddr::V4(addr) = remote.ip() else {
+            return Err(Error::new(ErrorKind::Other, "Only IPv4 supported"))
+        };
+        let remote = SocketAddr::new(addr.octets().into(), remote.port());
+        let soc = TcpStream::connect(remote)?;
+        soc.set_nonblocking(true)?;
+        self.stream.replace(soc);
+        Ok(())
+    }
+
+    fn stream_mut(&mut self) -> Result<&mut TcpStream, nb::Error<TcpError>> {
+        self.stream
+            .as_mut()
+            .ok_or_else(|| nb::Error::Other(TcpError::broken_pipe()))
+    }
+
+    pub fn close(&mut self) {
+        self.stream.take();
+    }
+}
+
+impl<'a> TcpClientStack for MitmStack<'a> {
+    type TcpSocket = TcpHandle;
+    type Error = TcpError;
+
+    fn socket(&mut self) -> Result<Self::TcpSocket, Self::Error> {
+        Ok(self.add_socket())
+    }
+
+    fn connect(
+        &mut self,
+        socket: &mut Self::TcpSocket,
+        remote: embedded_nal::SocketAddr,
+    ) -> nb::Result<(), Self::Error> {
+        let index = self
+            .sockets
+            .borrow_mut()
+            .iter()
+            .position(|(h, _sock)| h == socket)
+            .unwrap();
+        let socket = &mut self.sockets.borrow_mut()[index].1;
+        socket.connect(remote).map_err(to_nb)?;
+        Ok(())
+    }
+
+    fn send(
+        &mut self,
+        socket: &mut Self::TcpSocket,
+        buffer: &[u8],
+    ) -> nb::Result<usize, Self::Error> {
+        let index = self
+            .sockets
+            .borrow_mut()
+            .iter()
+            .position(|(h, _sock)| h == socket)
+            .unwrap();
+        let socket = &mut self.sockets.borrow_mut()[index].1;
+
+        let socket = socket.stream_mut()?;
+        socket.write(buffer).map_err(to_nb)
+    }
+
+    fn receive(
+        &mut self,
+        socket: &mut Self::TcpSocket,
+        buffer: &mut [u8],
+    ) -> nb::Result<usize, Self::Error> {
+        let index = self
+            .sockets
+            .borrow_mut()
+            .iter()
+            .position(|(h, _sock)| h == socket)
+            .unwrap();
+        let socket = &mut self.sockets.borrow_mut()[index].1;
+        let socket = socket.stream_mut()?;
+        socket.read(buffer).map_err(to_nb)
+    }
+
+    fn close(&mut self, socket: Self::TcpSocket) -> Result<(), Self::Error> {
+        self.close(socket);
+        Ok(())
+    }
+}
